0834e2863a13fda810f853ed4bdbf8e6f4a6b304
Kevin Morris <kevr.gtalk@gmail.com>
2020-08-09 03:19:31 -0700

    Toggle keyboard backlight with -t
    Checking for fopen failures really needs to be streamlined
    into a more reused function, if possible. Unfortunately with C,
    we require a lot of freeing for the path strings we create.
    
    Signed-off-by: Kevin Morris <kevr.gtalk@gmail.com>

--- src/main.c
+++ src/main.c
@@ -3,18 +3,25 @@
 #include <string.h>
 #include <unistd.h>
 
-#define SYSFS_PREFIX "/sys/class/leds/system76::kbd_backlight/color_"
+#define SYSFS_READ_FAILED 2
+#define SYSFS_WRITE_FAILED 3
 
-char *sysfs_path(const char *pos);
+#define SYSFS_PREFIX "/sys/class/leds/system76::kbd_backlight"
+#define SYSFS_COLOR_PREFIX SYSFS_PREFIX "/color_"
+
+char *join(const char *prefix, const char *value);
+
 int print_usage(int argc, char *argv[]);
 int print_help(int argc, char *argv[]);
+int error(int return_code, const char *message);
 
 int main(int argc, char *argv[])
 {
     int opt;
     char *left = NULL, *center = NULL, *right = NULL, *extra = NULL;
+    char toggle = 0;
 
-    while ((opt = getopt(argc, argv, "hl:c:r:e:")) != -1) {
+    while ((opt = getopt(argc, argv, "hl:c:r:e:t")) != -1) {
         switch (opt) {
         case 'h':
             return print_help(argc, argv);
@@ -30,9 +37,74 @@ int main(int argc, char *argv[])
         case 'e':
             extra = optarg;
             break;
+        case 't':
+            toggle = 1;
+            break;
         case '?':
             return print_usage(argc, argv);
         }
+    }
+
+    if (toggle) {
+        char buf[5];
+        char *brightness = join(SYSFS_PREFIX, "/brightness");
+
+        FILE *ifs = fopen(brightness, "r");
+        if (!ifs) {
+            free(brightness);
+            return error(SYSFS_READ_FAILED,
+                         "unable to open sysfs for reading; are you root?");
+        }
+        size_t bytes = fread(buf, sizeof(char), 4, ifs);
+        fclose(ifs);
+        buf[bytes] = '\0';
+
+        // Strip \n from the end of our input by overriding it with \0.
+        while (buf[bytes - 1] == '\n')
+            buf[--bytes] = '\0';
+
+        if (strcmp(buf, "0") == 0) {
+            // Get cached hw_brightness
+            char *hw_brightness = join(SYSFS_PREFIX, "/brightness_hw_changed");
+
+            ifs = fopen(hw_brightness, "r");
+            if (!ifs) {
+                free(brightness);
+                free(hw_brightness);
+                return error(
+                    SYSFS_READ_FAILED,
+                    "unable to open sysfs for reading; are you root?");
+            }
+            bytes = fread(buf, sizeof(char), 4, ifs);
+            fclose(ifs);
+            buf[bytes] = '\0';
+
+            // Restore the original brightness.
+            ifs = fopen(brightness, "w");
+            if (!ifs) {
+                free(brightness);
+                free(hw_brightness);
+                return error(
+                    SYSFS_WRITE_FAILED,
+                    "unable to open sysfs for writing; are you root?");
+            }
+            fwrite(buf, sizeof(char), bytes, ifs);
+            fclose(ifs);
+
+            free(hw_brightness);
+        } else {
+            // Turn it off; brightness is on!
+            ifs = fopen(brightness, "w");
+            if (!ifs) {
+                free(brightness);
+                return error(
+                    SYSFS_WRITE_FAILED,
+                    "unable to open sysfs for writing; are you root?");
+            }
+            fwrite("0", sizeof(char), 1, ifs);
+            fclose(ifs);
+        }
+        free(brightness);
     }
 
     const char *arr[] = {"left", "center", "right", "extra"};
@@ -41,8 +113,14 @@ int main(int argc, char *argv[])
     int i;
     for (i = 0; i < 4; ++i) {
         if (colors[i] != NULL) {
-            char *p = sysfs_path(arr[i]);
+            char *p = join(SYSFS_COLOR_PREFIX, arr[i]);
             FILE *ofs = fopen(p, "w");
+            if (!ofs) {
+                free(p);
+                return error(
+                    SYSFS_WRITE_FAILED,
+                    "unable to open sysfs for writing; are you root?");
+            }
             fwrite(colors[i], sizeof(char), 8, ofs);
             fwrite("\n", sizeof(char), 1, ofs);
             fclose(ofs);
@@ -53,21 +131,22 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-char *sysfs_path(const char *pos)
+char *join(const char *prefix, const char *value)
 {
-    const int prefix_len = strlen(SYSFS_PREFIX);
-    const int pos_len = strlen(pos);
+    const int prefix_len = strlen(prefix);
+    const int value_len = strlen(value);
 
-    char *buf = (char *)malloc(prefix_len + pos_len + 1);
-    memset(buf, 0, prefix_len + pos_len + 1);
-    strncpy(buf, SYSFS_PREFIX, prefix_len);
-    strncpy(&buf[prefix_len], pos, pos_len);
+    char *buf = (char *)malloc(prefix_len + value_len + 1);
+    memset(buf, 0, prefix_len + value_len + 1);
+    strncpy(buf, prefix, prefix_len);
+    strncpy(&buf[prefix_len], value, value_len);
+
     return buf;
 }
 
 int print_usage(int argc, char *argv[])
 {
-    printf("usage: %s [-hlcre]\n", argv[0]);
+    printf("usage: %s [-hlcret]\n", argv[0]);
     return -1;
 }
 
@@ -76,7 +155,13 @@ int print_help(int argc, char *argv[])
     print_usage(argc, argv);
     printf("\nProgram options:\n -h\t| Print help.\n -l\t| Left color "
            "(rgb).\n -c\t| Center color (rgb).\n -r\t| Right color (rgb).\n "
-           "-e\t| Extra color (rgb).\n\n");
+           "-e\t| Extra color (rgb).\n -t\t| Toggle keyboard light (on or "
+           "off).\n\n");
     return 0;
 }
 
+int error(int return_code, const char *message)
+{
+    fprintf(stderr, "error: %s\n", message);
+    return return_code;
+}
