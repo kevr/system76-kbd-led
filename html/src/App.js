03977cee838fe70d96b9d4adf2edab308b083f65
Kevin Morris <kevr.gtalk@gmail.com>
2020-08-20 02:05:09 -0700

    add react-color to html
    Signed-off-by: Kevin Morris <kevr.gtalk@gmail.com>

--- html/src/App.js
+++ html/src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
+import { BlockPicker } from 'react-color';
 import './App.css';
 
 function App() {
+  const [ color, setColor ] = useState("#ff0000");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BlockPicker
+        color={color}
+        onChangeComplete={e => {
+          setColor(e.hex);
+        }}
+      />
     </div>
   );
 }
