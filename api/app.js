6e50f21c7357d4032facefa7b148650c684ee209
Kevin Morris <kevr.gtalk@gmail.com>
2020-08-25 04:51:55 -0700

    add /colors and /colors/:region api route
    Signed-off-by: Kevin Morris <kevr.gtalk@gmail.com>

--- api/app.js
+++ api/app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -14,6 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
